0de5f959b0836f326123d7bb9c78b928f679422b
Kyle Gordon <kyle@glasgownet.com>
2024-02-21 21:18:49 +0000

    I really should use K3S or Rancher...

--- pull.sh
+++ pull.sh
@@ -1,4 +1,16 @@
-docker-compose -f ha-stack/docker-compose.yaml -H ssh://bagpuss@homeauto.vpn.glasgownet.com pull
-docker-compose -f media-stack/docker-compose.yaml -H ssh://bagpuss@homeauto.vpn.glasgownet.com pull
-docker-compose -f elk-stack/docker-compose.yaml -H ssh://bagpuss@homeauto.vpn.glasgownet.com pull
-
+echo "Pulling core stack"
+docker-compose -H ssh://bagpuss@172.24.32.13 -f core-stack/docker-compose.yaml pull
+echo "Pulling FrontEnd stack"
+docker-compose -H ssh://bagpuss@172.24.32.13 --env-file frontend/docker.env -f frontend/docker-compose.yaml pull
+echo "Pulling HA stack"
+docker-compose -H ssh://bagpuss@172.24.32.13 -f ha-stack/docker-compose.yaml pull
+echo "Pulling media stack"
+docker-compose -H ssh://bagpuss@172.24.32.13 -f media-stack/docker-compose.yaml pull
+echo "Pulling elk stack"
+docker-compose -H ssh://bagpuss@172.24.32.13 -f elk-stack/docker-compose.yaml pull
+echo "Pulling monitoring stack"
+docker-compose -H ssh://bagpuss@172.24.32.13 -f monitoring-stack/docker-compose.yaml pull
+echo "Pulling deepcore stacks"
+docker-compose -H ssh://bagpuss@deepcore.glasgownet.com --env-file frontend/docker-deepcore.env -f frontend/docker-compose-deepcore.yaml pull
+docker-compose -H ssh://bagpuss@deepcore.glasgownet.com -f pixelfed/docker-compose.yaml pull
+docker-compose -H ssh://bagpuss@deepcore.glasgownet.com -f social-stack/docker-compose.yaml pull
--- up.sh
+++ up.sh
@@ -1,3 +1,10 @@
-docker-compose -f ha-stack/docker-compose.yaml -H ssh://bagpuss@172.24.32.13 up -d
-docker-compose -f media-stack/docker-compose.yaml -H ssh://bagpuss@172.24.32.13 up -d
-#docker-compose -f elk-stack/docker-compose.yaml -H ssh://bagpuss@172.24.32.13 up -d
+docker-compose -H ssh://bagpuss@172.24.32.13 -f core-stack/docker-compose.yaml up -d
+docker-compose -H ssh://bagpuss@172.24.32.13 --env-file frontend/docker.env -f frontend/docker-compose.yaml up -d
+docker-compose -H ssh://bagpuss@deepcore.glasgownet.com --env-file frontend/docker-deepcore.env -f frontend/docker-compose-deepcore.yaml up -d
+docker-compose -H ssh://bagpuss@172.24.32.13 -f ha-stack/docker-compose.yaml up -d
+docker-compose -H ssh://bagpuss@172.24.32.13 -f media-stack/docker-compose.yaml up -d
+docker-compose -H ssh://bagpuss@172.24.32.13 -f monitoring-stack/docker-compose.yaml up -d
+docker-compose -H ssh://bagpuss@172.24.32.13 -f elk-stack/docker-compose.yaml up -d
+docker-compose -H ssh://bagpuss@172.24.32.13 -f elk-stack/docker-compose-deepcore.yaml up -d
+docker-compose -H ssh://bagpuss@deepcore.glasgownet.com -f social-stack/docker-compose.yaml up -d
+# docker-compose -H ssh://bagpuss@deepcore.glasgownet.com -f social-stack/pixelfed.yaml up -d
